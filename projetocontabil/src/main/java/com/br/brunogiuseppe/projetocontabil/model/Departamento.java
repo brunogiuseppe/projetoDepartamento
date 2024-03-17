9ff7bfcb871542859d60a2f7181d136f198a56a6
Bruno Giuseppe <103140386+brunogiuseppe@users.noreply.github.com>
2024-03-17 13:51:43 -0300

    Departamento.java

--- projetocontabil/src/main/java/com/br/brunogiuseppe/projetocontabil/model/Departamento.java
+++ projetocontabil/src/main/java/com/br/brunogiuseppe/projetocontabil/model/Departamento.java
@@ -1,4 +1,35 @@
 package com.br.brunogiuseppe.projetocontabil.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="DEPARTAMENTO")
 public class Departamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    // getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
